6a5297ef75434f5466177c97aefba58d868f33f5
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-06-09 08:57:08 +0300

    commit tree b35e1b6060f4240a94245fadbd4da4970a02763e

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // code your solution here
-function saturdayFun(activity= "roller-skate") {
+const saturdayFun= function(activity= "roller-skate") {
     return `This Saturday, I want to ${activity}!`
 } 
 
-function mondayWork(activity= "go to the office") {
+const mondayWork= function(activity= "go to the office") {
     return `This Monday, I will ${activity}.`;
 }
 
-function wrapAdjective(result = "*") {
+const wrapAdjective= function(result = "*") {
     return function(emphatic= 'a hard worker'){
         return `You are ${result}${emphatic}${result}!`
     }
